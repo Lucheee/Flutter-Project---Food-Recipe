e2acbb29386a87b40fc5ac7cd29129b7510eee29
Luchee <oluchie51@gmail.com>
2024-02-09 10:23:51 +0100

    Created a new Image Section

--- foodrecipeapp/lib/main.dart
+++ foodrecipeapp/lib/main.dart
@@ -39,16 +39,22 @@ class RecipeApp extends StatelessWidget {
         ),
       )),
       body: const Padding(
-        padding: EdgeInsets.all(35.0),
+        padding: EdgeInsets.symmetric(horizontal: 30.0),
         child: Center(
-          child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
             Text(
               "How to make french toast",
-              
               style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w600),
             ),
-             SizedBox(
-              height: 17,
+            SizedBox(
+              height: 10,
+            ),
+            ImageSection(
+              image: 'assets/images/image 13.png'
+            ),
+            SizedBox(
+              height: 20
             ),
             ReviewSection(),
             ProfileSection(
@@ -56,6 +62,7 @@ class RecipeApp extends StatelessWidget {
               name: "Roberta Anny",
               image: 'assets/images/Location.png',
             )
+          
           ]),
         ),
       ),
@@ -71,7 +78,7 @@ class ReviewSection extends StatelessWidget {
     return const Row(children: [
       Icon(Icons.star, color: Color.fromARGB(255, 251, 182, 10)),
       Text(
-        '41',
+        '4,5',
         style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
       ),
       Text(
@@ -101,27 +108,53 @@ class ProfileSection extends StatelessWidget {
           children: [
             Text(
               name,
-              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0),
+              style:
+                  const TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0),
             ),
 
             Row(
               children: [
-                Image.network(
+                Image.asset(
                   image,
                   width: 20.0,
                   height: 25.0,
                   fit: BoxFit.cover,
                 ),
-                const SizedBox(width: 5.0,),
-                Text(address, style: const TextStyle(fontSize: 18.0),)
+                const SizedBox(
+                  width: 5.0,
+                ),
+                Text(
+                  address,
+                  style: const TextStyle(fontSize: 18.0),
+                )
               ],
             ),
-            // const SizedBox(width: 5,
-            // height: 10
-            // ),
+            const SizedBox(
+            height: 10
+            ),
           ],
         ),
       ],
     );
+  }
+}
+
+class ImageSection extends StatelessWidget {
+  const ImageSection({super.key, required this.image});
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+    Center(
+      child: ClipRRect(
+        borderRadius:  BorderRadius.circular(20.0),
+        child : Image.asset(image, width:400, height:200, fit: BoxFit.cover),
+         ),   
+         );
+    
+  
+  
   }
 }
